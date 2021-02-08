eed5ca4ee34c573ddaf0bb540f95e42dd8e7b82e
danielpoliveira <dani.edm@outlook.com>
2021-02-08 10:13:34 -0300

    Usando estrutura de loops

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,48 @@
 fun main(args: Array<String>) {
     println("Bem vindo ao ByteBank")
 
-    val titular: String = "Daniel"
-    val numeroConta: Int = 7500
-    var saldo: Double = -20.0
+//    for (i in 5 downTo 1) {
+//
+//        if(i == 4) {
+//            continue
+//        }
+//
+//        val titular: String = "Daniel $i"
+//        val numeroConta: Int = 7500 + i
+//        var saldo: Double = i + 10.0
+//
+//        println("Titular: $titular")
+//        println("Número da conta: $numeroConta")
+//        println("Saldo disponível: $saldo")
+//
+//        println("Valor de i: $i")
+//
+//        println("=================")
+//        testaCondicoes(saldo)
+//    }
 
-//    saldo = 100 + 2.0
-//    saldo += 200
+    var i: Int = 0
+    while (i < 5) {
+        val titular: String = "Daniel $i"
+        val numeroConta: Int = 7500 + i
+        var saldo: Double = i + 10.0
 
-    println("Titular: $titular")
-    println("Número da conta: $numeroConta")
-    println("Saldo disponível: $saldo")
+        println("Titular: $titular")
+        println("Número da conta: $numeroConta")
+        println("Saldo disponível: $saldo")
 
+        println("Valor de i: $i")
+
+        println("=================")
+
+        i++
+    }
+}
+
+fun testaCondicoes(saldo: Double) {
     when {
         saldo > 0.0 -> println("Conta é positiva")
         saldo == 0.0 -> println("Conta é neutra")
         else -> println("Conta é negativa")
     }
-
 }
