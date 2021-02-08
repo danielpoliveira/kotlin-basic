2356cf157d3962f0c9c7cf92de292a4925387afa
danielpoliveira <dani.edm@outlook.com>
2021-02-08 20:17:29 -0300

    Atribuição de valores a uma variável por cópia e por referência

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,57 @@
 fun main(args: Array<String>) {
-    println("Bem vindo ao ByteBank")
+//    println("Bem vindo ao ByteBank")
+//    println()
+//
+//    val contaDaniel = Conta();
+//    val contaFran = Conta()
+//
+//    contaDaniel.titular = "Daniel"
+//    contaDaniel.numero = 7500
+//    contaDaniel.saldo = 1600.0
+//
+//    contaFran.titular = "Fran"
+//    contaFran.numero = 9751
+//    contaFran.saldo = 350.45
+//
+//    println(contaDaniel.titular)
+//    println(contaDaniel.numero)
+//    println(contaDaniel.saldo)
+//
+//    println()
+//
+//    println(contaFran.titular)
+//    println(contaFran.numero)
+//    println(contaFran.saldo)
 
-//    for (i in 5 downTo 1) {
+    val numeroX = 10
+    var numeroY = numeroX
+
+    numeroY++
+
+    println("NúmeroX: $numeroX")
+    println("NúmeroY: $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+
+    var contaMaria = Conta()
+    contaMaria.titular = "Maria"
+
+    println("Conta titular joao: ${contaJoao.titular}")
+    println("Conta titular maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaLacos() {
+    //    for (i in 5 downTo 1) {
 //
 //        if(i == 4) {
 //            continue
