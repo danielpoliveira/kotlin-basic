dcf4bcd9a2c56df87298936211836aa3ecf0a230
danielpoliveira <dani.edm@outlook.com>
2021-02-09 10:01:16 -0300

    Utilizando herança

--- src/main/kotlin/Funcionario.kt
+++ src/main/kotlin/Funcionario.kt
@@ -1,24 +1,9 @@
-class Funcionario(
+open class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double,
-    val tipo: Int //0 Funcionário comum, 1 Gerente, 2 Diretor...
+    val salario: Double
 ){
     fun bonificacao(): Double {
-        when (this.tipo) {
-            0 -> {
-                return salario * 0.1
-            }
-            1 -> {
-                return salario * 0.2
-            }
-            else -> {
-                return salario * 0.3
-            }
-        }
-    }
-
-    fun autentica(senha: Int) {
-
+        return salario * 0.1
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,55 @@
 fun main(args: Array<String>) {
-    println("Bem vindo ao ByteBank\n")
+    println("Bem vindo ao ByteBank")
 
     val daniel = Funcionario(
         nome = "Daniel",
         cpf = "123.456.789-10",
         salario = 15648.45,
-        tipo = 0
     )
 
+    println()
     println("Nome: ${daniel.nome}")
     println("CPF: ${daniel.cpf}")
     println("Salário: ${daniel.salario}")
     println("Bonificação: ${daniel.bonificacao()}")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "234.567.890.11",
+        salario = 36978.45,
+        senha = 1234
+    )
+
+    println()
+    println("Nome: ${fran.nome}")
+    println("CPF: ${fran.cpf}")
+    println("Salário: ${fran.salario}")
+    println("Bonificação: ${fran.bonificacao()}")
+
+    if(fran.autentica(1234)) {
+        println("Autenticou com sucesso :)")
+    } else {
+        println("Falha na autenticação :(")
+    }
+
+    val gui = Diretor(
+        nome="Gui",
+        cpf = "345.678.901.-12",
+        salario = 98457.97,
+        senha=9876,
+        plr = 200.0
+    )
+
+    println()
+    println("Nome: ${gui.nome}")
+    println("CPF: ${gui.cpf}")
+    println("Salário: ${gui.salario}")
+    println("Bonificação: ${gui.bonificacao()}")
+    println("Partificação lucro: ${gui.plr}")
+
+    if(gui.autentica(9876)) {
+        println("Autenticou com sucesso :)")
+    } else {
+        println("Falha na autenticação :(")
+    }
 }
