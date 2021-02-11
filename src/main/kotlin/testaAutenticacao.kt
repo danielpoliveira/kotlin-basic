c244ecb0954ef6d1ec0ca7e1520ddab748ad32b7
danielpoliveira <dani.edm@outlook.com>
2021-02-10 22:21:25 -0300

    Implementando autenticação com interfaces

--- src/main/kotlin/testaAutenticacao.kt
+++ src/main/kotlin/testaAutenticacao.kt
@@ -14,8 +14,15 @@ fun testaAutenticacao() {
         plr = 200.00
     )
 
+    val cliente = Cliente(
+        nome = "Anna",
+        cpf = "465.464.646-51",
+        senha = 7777
+    )
+
     val sistemaInterno = SistemaInterno()
 
     sistemaInterno.entra(gerente, 1234)
     sistemaInterno.entra(diretora, 9876)
+    sistemaInterno.entra(cliente, 7777)
 }
